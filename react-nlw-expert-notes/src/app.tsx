4a74640980677a9b564e73388118b66957111c1a
Gonçalo Fonseca <goncalofonseca2003.g@gmail.com>
2024-07-21 18:15:01 +0100

    feat: all initial config done

---
+++ react-nlw-expert-notes/src/app.tsx
@@ -0,0 +1,15 @@
+import  logo  from '../src/assets/logo-nlw-expert.svg'
+
+
+export function App() {
+  return (
+    <div className='mx-auto max-w-6xl my-12'> 
+      
+    <img src={logo} alt="" /> 
+
+    <input type="text" placeholder='Procure as suas notas' />
+    
+    </div>
+  )
+}
+
